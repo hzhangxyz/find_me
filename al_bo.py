77149cee6a1e3d91333b00f6ffeaff3095006fee
trumpet <zh19970205@126.com>
2016-07-27 23:39:11 +0000

    al bo

--- al_bo.py
+++ al_bo.py
@@ -4,3 +4,19 @@ with open("output","r") as out_file:
 data="[%s]"%data.replace("\n",",")
 data=[[i["point"], i["value"]] for i in eval(data)]
 to_count=[i[0] for i in data]
+
+l=len(to_count[0])
+
+def which_box(sample,r):
+    p=[int(i/r) for i in sample]
+    return str(p)
+
+def box_count(e):
+    n=2**e
+    r=5./n
+    d=set([])
+    for i in to_count[:10]:
+        d.add(which_box(i,r))
+    return len(d)
+
+print map(box_count,range(10))
