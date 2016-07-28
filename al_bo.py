e121cb539334bb8389fb2008cab8ddf674a4bda6
trumpet <zh19970205@126.com>
2016-07-28 00:58:31 +0000

    al bo

--- al_bo.py
+++ al_bo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 with open("output","r") as out_file:
     data=out_file.read()
-data="[%s]"%data.replace("\n",",")
+#data="[%s]"%data.replace("\n",",")
 data=[[i["point"], i["value"]] for i in eval(data)]
 to_count=[i[0] for i in data]
 
