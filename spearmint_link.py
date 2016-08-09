d7f7a7d4866249b483ab2c7c5b5bba10486e4939
trumpet <zh19970205@126.com>
2016-08-09 14:01:33 +0000

    spear link

--- spearmint_link.py
+++ spearmint_link.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from shared import get_energy
+from shared import get_energy,sym_table,dim
 
 def main(id,param):
-    return get_energy([param["a"],param["b"],param["c"]],0,id)
+    return get_energy([param[sym_table[i]] for i in range(dim)],0,id)
