9ddd86148167623e480982417a35f88e35a26beb
trumpet <zh19970205@126.com>
2016-08-09 08:35:22 +0000

    pso

--- pso_find_me.py
+++ pso_find_me.py
@@ -2,7 +2,9 @@
 import os
 import random
 import mpi4py.MPI as MPI
-from shared import *
+from shared import get_energy,l,sym_region,times,omega,phip,phig
+
+os.system("rm try* -rf")
 
 comm = MPI.COMM_WORLD
 comm_rank = comm.Get_rank()
@@ -21,14 +23,18 @@ GE=min(PG)
 best=PG.index(GE)
 G=comm.bcast(S if comm_rank == best else None, root=best)
 DEBUG=os.getenv("DEBUG")=="T"
+
 if DEBUG:
-    to_print_S=comm.gather(S, root=0)
-    to_print_temp=comm.gather(PE, root=0)
+    combine_S=comm.gather(S,root=0)
+    combine_E=comm.gather(PE,root=0)
     if comm_rank==0:
-        print [to_print_S,to_print_temp]
-elif comm_rank==0:
-    print G
-    print GE
+        for j in range(comm_size):
+            print combine_S[j]
+            print combine_E[j]
+else:
+    if comm_rank==0:
+        print G
+        print GE
 
 #pso!
 
@@ -41,20 +47,26 @@ for i in range(times):
     S=[S[j] if S[j]<sym_region[j][1] else sym_region[j][1] for j in range(l)]
     S=[S[j] if S[j]>sym_region[j][0] else sym_region[j][0] for j in range(l)]
     temp=get_energy(S,comm_rank,i)
+    # p
     if(temp<PE):
         P=[S[i] for i in range(l)]
         PE=temp
+    # g
     PG=comm.allgather(PE)
     g_temp=min(PG);
     if(g_temp<GE):
         GE=g_temp
         best=PG.index(g_temp)
         G=comm.bcast(S if comm_rank == best else None, root=best)
+    # output
     if DEBUG:
-        to_print_S=comm.gather(S, root=0)
-        to_print_temp=comm.gather(temp, root=0)
+        combine_S=comm.gather(S,root=0)
+        combine_E=comm.gather(temp,root=0)
         if comm_rank==0:
-            print [to_print_S,to_print_temp]
-    elif comm_rank==0:
-        print G
-        print GE
+            for j in range(comm_size):
+                print combine_S[j]
+                print combine_E[j]
+    else:
+        if comm_rank==0:
+            print G
+            print GE
