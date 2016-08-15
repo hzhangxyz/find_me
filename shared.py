2bb7465a4991366477ef9bd99d75be9ee9a47cd3
trumpet <zh19970205@126.com>
2016-08-15 17:25:09 +0000

    update

--- shared.py
+++ shared.py
@@ -1,3 +1,45 @@
+scale = 1
+precision = 2
+nums = 3
+title = "H2O"
+atoms = "H O"
+num = "2 1"
+var = [1,-0.3,0.9]
+
+def write_poscar(scale,precision,nums,title,atoms,num,var):
+    p=precision
+    cys = "%%.%df %%.%df %%.%df\n%%.%df %%.%df %%.%df\n%%.%df %%.%df %%.%df"%(
+        p,p,p,p,p,p,p,p,p
+    )%(
+        scale*5,0,0,
+        0,scale*5,0,
+        0,0,scale*5
+    )
+    posi = ""
+    for i in range(nums):
+        if i == 0:
+            posi+="%%.%df %%.%df %%.%df F F F\n"%(p,p,p)%(
+                0,0,0)
+        elif i == 1:
+            posi+="%%.%df %%.%df %%.%df T F F\n"%(p,p,p)%(
+                var[0],0,0)
+        elif i == 2:
+            posi+="%%.%df %%.%df %%.%df T T F\n"%(p,p,p)%(
+                var[1],var[2],0)
+        else:
+            posi+="%%.%df %%.%df %%.%df T T T\n"%(p,p,p)%(
+                var[i*3-6],var[i*3-4],var[i*3-3])
+    poscar = "%s\n%s\n%s\n%s\n%s\nS\nC\n%s"%(
+        title,
+        "%%.%df"%precision%1,
+        cys,
+        atoms,
+        num,
+        posi
+    )
+    with open("POSCAR","w") as posc:
+        posc.write(poscar)
+
 def get_single_point(src,pos):
     test=src.find("POSITION",pos)
     if test == -1:
@@ -18,7 +60,8 @@ def get_single_point(src,pos):
             position.append(pre_position[i*6+0])
             position.append(pre_position[i*6+1])
             position.append(pre_position[i+8+2])
-    position=tuple(map(float,position))
+    position=map(float,position)
+    position=map(lambda x:x if x<scale/2. else x-scale,position)
     pre_en=src.find("=",ender)
     en=float(src[pre_en+1:src.find("eV",pre_en)].split()[0])
     return position,en,pre_en
