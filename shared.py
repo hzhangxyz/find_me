2b60f95e45d701cce610c71d978e00be61178a80
trumpet <zh19970205@126.com>
2016-08-16 03:34:52 +0000

    try

--- shared.py
+++ shared.py
@@ -1,3 +1,6 @@
+import shutil
+import os
+
 scale = 1
 precision = 2
 nums = 3
@@ -5,8 +8,10 @@ title = "H2O"
 atoms = "H O"
 num = "2 1"
 var = [1,-0.3,0.9]
+tag = 0
 
-def write_poscar(scale,precision,nums,title,atoms,num,var):
+def run_vasp():
+    # Generate POSCAR
     p=precision
     cys = "%%.%df %%.%df %%.%df\n%%.%df %%.%df %%.%df\n%%.%df %%.%df %%.%df"%(
         p,p,p,p,p,p,p,p,p
@@ -37,36 +42,45 @@ def write_poscar(scale,precision,nums,title,atoms,num,var):
         num,
         posi
     )
-    with open("POSCAR","w") as posc:
+    # Copy Files
+    name="try_%d"%tag
+    shutil.rmtree(name,ignore_errors=True)
+    os.makedirs(name)
+    with open("%s/POSCAR"%name,"w") as posc:
         posc.write(poscar)
-
-def get_single_point(src,pos):
-    test=src.find("POSITION",pos)
-    if test == -1:
-        return None
-    starter=src.find("\n",src.find("\n",test)+1)
-    ender=src.find("\n -",starter)
-    pre_position=src[starter:ender].split()
-    position=[]
-    for i in range(len(pre_position)/6):
-        if i==0:
-            pass
-        elif i==1:
-            position.append(pre_position[i*6+0])
-        elif i==2:
-            position.append(pre_position[i*6+0])
-            position.append(pre_position[i*6+1])
-        else:
-            position.append(pre_position[i*6+0])
-            position.append(pre_position[i*6+1])
-            position.append(pre_position[i+8+2])
-    position=map(float,position)
-    position=map(lambda x:x if x<scale/2. else x-scale,position)
-    pre_en=src.find("=",ender)
-    en=float(src[pre_en+1:src.find("eV",pre_en)].split()[0])
-    return position,en,pre_en
-
-def get_all_points(src):
+    shutil.copy("POTCAR","%s/POTCAR"%name)
+    shutil.copy("INCAR","%s/INCAR"%name)
+    shutil.copy("KPOINTS","%s/KPOINTS"%name)
+    os.system("cd %s;vasp_without_mpi 1>output"%name)
+    with open("%s/OUTCAR"%name,"r") as outc:
+        src=outc.read()
+    # Define Get Single Energy
+    def get_single_point(src,pos):
+        test=src.find("POSITION",pos)
+        if test == -1:
+            return None
+        starter=src.find("\n",src.find("\n",test)+1)
+        ender=src.find("\n -",starter)
+        pre_position=src[starter:ender].split()
+        position=[]
+        for i in range(len(pre_position)/6):
+            if i==0:
+                pass
+            elif i==1:
+                position.append(pre_position[i*6+0])
+            elif i==2:
+                position.append(pre_position[i*6+0])
+                position.append(pre_position[i*6+1])
+            else:
+                position.append(pre_position[i*6+0])
+                position.append(pre_position[i*6+1])
+                position.append(pre_position[i+8+2])
+        position=map(float,position)
+        position=map(lambda x:x if x<scale/2. else x-scale,position)
+        pre_en=src.find("=",ender)
+        en=float(src[pre_en+1:src.find("eV",pre_en)].split()[0])
+        return position,en,pre_en
+    # Get Energy
     pos=0
     ans=[get_single_point(src,pos)]
     while ans[-1]!=None:
