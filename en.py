45c6fc975600ef88cb4f48f965486b20d12a3c06
trumpet <zh19970205@126.com>
2016-08-21 04:43:44 +0000

    en

--- en.py
+++ en.py
@@ -4,4 +4,4 @@ string="cat ./try_%d/OUTCAR | grep TOTEN | tail -n 1 2>/dev/null"
 for i in range(100):
     ans = sp.check_output(string%i,shell=True).replace("\n","")
     if ans != "":
-        print ans
+        print "%d\t%s"%(i,ans)
