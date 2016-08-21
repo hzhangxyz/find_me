7f4b4fa37636617341c061c2af82dbc5d0df6bd4
trumpet <zh19970205@126.com>
2016-08-21 02:29:53 +0000

    tag  number

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ title = "CH4"
 atoms = "C H"
 num = "1 4"
 #var = [1.09,-0.36,1.03,-0.36,-0.34,0.97,-0.36,-0.34,-0.97]
-tag = 0
+#tag = 0
 
 def main(s_id,pre_var):
+    tag=s_id
     var = map(lambda x:pre_var[x],"x1 x2 x3 x4 x5 x6 x7 x8 x9".split())
     pos = []
     for i in range(len(var)/3+2):
