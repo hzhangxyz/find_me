5279058e621675e5575deebf48a617022261549a
trumpet <zh970205@outlook.com>
2017-03-17 15:47:49 +0800

    remove-useless-things

--- run_spearmint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-killall mongod -9
-rm ~/var/mongodb/* -rf
-mongod --fork --logpath ~/var/mongodb/log --dbpath ~/var/mongodb
-
-source activate spearmint
-python ~/envs/spearmint/lib/python2.7/site-packages/spearmint/main.py .
-source deactivate
--- spearmint_link.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#
-
-from shared import get_energy,sym_table,dim
-
-def main(id,param):
-    print param
-    return get_energy([param[sym_table[i]] for i in range(dim)],0,id)
